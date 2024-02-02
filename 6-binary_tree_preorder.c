226c512e31ac3a13b664c33217f2977de04300e4
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-02 13:42:44 -0800

    all here

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,7 +5,7 @@
  * @tree: the root node's pointer
  * @func: the function's pointer for calling each node
  */
-void binary_tree_preorder(const binaryy_tree_t *tree, void (*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
