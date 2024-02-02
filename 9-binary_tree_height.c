85be254d37e96c2761c2b17adb1d99485fcad9b1
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-02 14:43:40 -0800

    all here

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t x = 0, y = 0;
-	
+
 		x = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		y = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		if (x > y)
