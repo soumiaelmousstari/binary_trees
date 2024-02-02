3537ae7df219aa87bce5bd3fe0de878fe7f01d9e
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-02 15:14:42 -0800

    all here

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size -done the the binary tree's size
+ * @tree: the binary tree for doning its size
+ * Return: the tree's size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t tree_size = 0;
+
+	if (tree)
+	{
+		tree_size += 1;
+		tree_size += binary_tree_size(tree->left);
+		tree_size += binary_tree_size(tree->right);
+	}
+	return (size);
+}
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves -done the leaves of binary tree
+ * @tree: the tree to done the leaves
+ * Return: the number of leaves found
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t tree_leaves = 0;
+
+	if (tree)
+	{
+		tree_leaves += (!tree->left && !tree->right) ? 1 : 0;
+		tree_leaves += binary_tree_leaves(tree->left);
+		tree_leaves += binary_tree_leaves(tree->right);
+	}
+	return (leaves);
+}
