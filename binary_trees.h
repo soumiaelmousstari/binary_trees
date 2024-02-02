6cee95096c127e6fecfc0b1ca160125b161d341a
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-02 15:58:39 -0800

    all here

--- binary_trees.h
+++ binary_trees.h
@@ -37,6 +37,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
 
 #endif
