0905b3792407fe093684066a9b9b5717b44af2e1
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-02 15:37:06 -0800

    all here

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,5 +15,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		tree_nodes += binary_tree_nodes(tree->left);
 		tree_nodes += binary_tree_nodes(tree->right);
 	}
-	return (nodes);
+	return (tree_nodes);
 }
